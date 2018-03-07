c0afb1daa47d06cf8164af4b59f93566c23231df
BINOTTO BRAGA Amanda <amanda.binotto-braga@ensimag.grenoble-inp.fr>
2018-03-07 11:43:34 +0100

    Fichiers squelets pour la phase 4

---
+++ kernel/fileMessage.c
@@ -0,0 +1,21 @@
+#define NBQUEUE 20
+
+
+
+
+//pcreate : crée une file de messages
+
+
+//pdelete : détruit une file de messages
+
+
+//psend : dépose un message dans une file
+
+
+//preceive : retire un message d'une file
+
+
+//preset : réinitialise une file
+
+
+//pcount : renvoie l'état courant d'une file
