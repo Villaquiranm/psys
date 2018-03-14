6da1b304f777791bfdbe77891024cd53494b3b63
Leonardo Almeida da Silveira <Leonardo.Almeida-Da-Silveir@ensimag.grenoble-inp.fr>
2018-03-14 12:30:47 +0100

    fonctions de traitement de messages

--- kernel/fileMessage.c
+++ kernel/fileMessage.c
@@ -63,12 +63,99 @@ int pdelete(int fid) {
 }
 
 //psend : dépose un message dans une file
+int psend(int fid, int message){
+
+  //valider FID
+  if(fid < 0 || fid > NBQUEUE-1 || queues[fid] == NULL)
+    return -1;
+
+  int nbMsgs = queues[fid]->numberMessages;
+  int capacite = queues[fid]->capacite;
+
+  //défensive, ce cas ne doit jamais arriver
+  if(nbMsgs > capacite || nbMsgs < 0)
+    return -1;
+
+  //si la file est vide
+  //et que des processus sont bloqués en attente de message,
+  //alors le processus le plus ancien dans la file parmi les plus prioritaires est débloqué et reçoit ce message
+  if(nbMsgs == 0 /*TODO && il y a des procs bloques*/){
+
+  }
+
+  //si la file est pleine,
+  //le processus appelant passe dans l'état bloqué sur file pleine
+  //jusqu'à ce qu'une place soit disponible dans la file pour y mettre le message.
+  //Il est possible également, qu'après avoir été mis dans l'état bloqué sur file pleine,
+  //le processus soit remis dans l'état activable par un autre processus ayant exécuté preset ou pdelete.
+  //Dans ce cas, la valeur de retour de psend est strictement négative.
+  else
+  if(nbMsgs == capacite){
+
+  }
+
+  //Sinon, la file n'est pas pleine et aucun processus n'est bloqué en attente de message.
+  //Le message est alors déposé directement dans la file.
+  else{
+    queues[fid]->message[nbMsgs] = message;
+    queues[fid]->numberMessages++;
+  }
+
+  return 0;
+}
+
+
+//TODO la liste de messages ne peut pas être un vecteur, parce qu'on doit lire
+//l'élement le plus ancien e le rétirer
+//idée: utiliser l'API de link une fois qu'elle est comprise
+//ou créer une liste enchaînée pour les messages
 
 
 //preceive : retire un message d'une file
+int preceive(int fid,int *message){
 
+  //valider FID
+  if(fid < 0 || fid > NBQUEUE-1 || queues[fid] == NULL)
+    return -1;
 
+  int nbMsgs = queues[fid]->numberMessages;
+  int capacite = queues[fid]->capacite;
+
+  //défensive, ce cas ne doit jamais arriver
+  if(nbMsgs > capacite || nbMsgs < 0)
+    return -1;
+
+  //si la file est vide, bloque le proc
+  if(nbMsgs == 0){
+
+  }
+
+  //sinon, il y a un message à lire
+  else{
+
+    //lit le message et le supprime de la file
+    *message = 0;//TODO pas 0, getOldestMessageAndDeleteIt;
+
+    //si la file était pleine, débloque un processus
+    if(nbMsgs == capacite){
+
+    }
+
+  }
+
+  return 0;
+}
+
+/*
 //preset : réinitialise une file
+int preset(int fid){
+  return -1;
+}
 
 
 //pcount : renvoie l'état courant d'une file
+int pcount(int fid, int *count){
+
+  return -1;
+}
+*/
